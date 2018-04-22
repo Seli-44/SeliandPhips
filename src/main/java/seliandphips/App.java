738935a91903d006558afe95e1612aefaf1385a1
seli <selina.schoenherr@gmx.de>
2018-04-22 09:41:26 +0200

    new feature

--- src/main/java/seliandphips/App.java
+++ src/main/java/seliandphips/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Phips ist nicht aufgeregt!!!!11!!!elf1!!11")
     }
 }
